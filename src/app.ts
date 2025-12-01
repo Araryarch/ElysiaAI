1445f067a5edaf560adf6f50c1c23d0ad6e79aed
Araryarch <araryaarch@gmail.com>
2025-12-01 13:58:35 +0700

    aaa

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import { Elysia } from "elysia";
 import chatRoute from "./routes/chat";
 import healthRoute from "./routes/health";
 import visionRoute from "./routes/vision";
+import modelsRoute from "./routes/models";
 
 export default new Elysia()
   .get("/", () => "I'm Good")
   .use(chatRoute)
   .use(healthRoute)
-  .use(visionRoute);
+  .use(visionRoute)
+  .use(modelsRoute);
