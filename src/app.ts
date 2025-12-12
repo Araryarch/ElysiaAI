d6b6533972b629ab74a120b4772d7c175b510081
Araryarch <araryaarch@gmail.com>
2025-12-12 14:18:25 +0700

    fix: fix

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 import chatRoute from "./routes/chat";
 import healthRoute from "./routes/health";
 import visionRoute from "./routes/vision";
 import modelsRoute from "./routes/models";
 
 export default new Elysia()
+  .use(cors())
   .get("/", () => "I'm Good")
   .use(chatRoute)
   .use(healthRoute)
